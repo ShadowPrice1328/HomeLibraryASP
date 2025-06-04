ea537d2e839dc76c54bdb6c0780e722b460fbc89
ShadowPrice1328 <kisukeurahara1328@gmail.com>
2025-06-04 02:32:09 +0200

    Completed Sorting and Searching

--- BookLibrary/Pages/Library.cshtml.cs
+++ BookLibrary/Pages/Library.cshtml.cs
@@ -19,14 +19,6 @@ public class LibraryModel : PageModel
     public List<Book> Books { get; set; }
     public int BooksTotalCount { get; set; }
 
-    public void OnGet()
-    {
-        _books = _bookRepository.ReadBooks();
-
-        Books = _books;
-        BooksTotalCount = _books.Count;
-    }
-    
     public IActionResult OnPostDelete(int id)
     {
         var book = _bookRepository.ReadBook(id);
@@ -37,6 +29,39 @@ public class LibraryModel : PageModel
 
         _bookRepository.DeleteBook(id);
         return RedirectToPage("/Library");
+    }
+
+    public void OnGet(string? sortBy, string? query)
+    {
+        _books = _bookRepository.ReadBooks();
+
+        if (!string.IsNullOrEmpty(query))
+        {
+            _books = _books.Where(b => !string.IsNullOrEmpty(b.Title) && b.Title.Contains(query, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+        }
+
+        if (!string.IsNullOrEmpty(sortBy))
+        {
+            switch (sortBy.ToLower())
+            {
+                case "title":
+                    _books = _books.OrderBy(b => b.Title).ToList();
+                    break;
+                case "year":
+                    _books = _books.OrderBy(b => b.Year).ToList();
+                    break;
+                case "author":
+                    _books = _books
+                        .OrderBy(b => b.IdAuthors.FirstOrDefault()?.LastName ?? "")
+                        .ThenBy(b => b.IdAuthors.FirstOrDefault()?.FirstName ?? "")
+                        .ToList();
+                    break;
+            }
+        }
+
+        Books = _books;
+        BooksTotalCount = _books.Count;
     }
 
 }
